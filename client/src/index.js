1c51727089d9db4c05c280660b076e17125fed42
Jack <ajackofsometrades@gmail.com>
2018-12-09 10:58:14 -0800

    a bit ahead of myself, but some initial apollo setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,25 @@ import { Provider } from 'react-redux';
 import App from './components/app/App.jsx';
 import store from './store';
 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
