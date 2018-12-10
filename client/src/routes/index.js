a6f106a3b416ea19fa0a5db556784cdf0872e4fa
Ryan Luras <ryanluras@gmail.com>
2018-12-10 12:28:34 -0800

    router cleanup

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from '../components/resources/about/About.jsx';
 import Home from '../components/resources/home/Home.jsx';
-// import Events from '../components/resources/events/Events.jsx';
-import PollList from '../components/resources/poll/PollList.jsx';
-import PollDetail from '../components/resources/poll/PollDetail.jsx';
-import PollCreate from '../components/resources/poll/PollCreate.jsx';
-import { Session } from '../components/lib/Session.jsx';
 import Auth from '../components/resources/auth/Auth.jsx';
+import SignUp from '../components/resources/signup/SignUp.jsx';
 
 export const ROUTES = {
   ABOUT: {
@@ -20,35 +16,9 @@ export const ROUTES = {
     Component: Home,
     linkTo: () => '/home',
   },
-  // EVENTS: {
-  //   path: '/events/',
-  //   Component: Events,
-  //   linkTo: () => '/events',
-  // },
-  POLLS: {
-    path: '/polls',
-    Component: PollList,
-    // Component: Session(PollList),
-    linkTo: () => '/polls'
-  },
-  POLL: {
-    path: '/polls/:id',
-    Component: PollDetail,
-    linkTo: id => `/polls/${id}`
-  },
-  CREATE_POLL: {
-    path: '/polls/create',
-    Component: PollCreate,
-    linkTo: () => '/polls/create'
-  },
-  //   AUTH: {
-  //     path: '/auth',
-  //     Component: Auth,
-  //     linkTo: () => '/auth'
-  //   }
   SIGNUP: {
     path: '/signup',
-    Component: Auth,
+    Component: SignUp,
     linkTo: () => '/signup'
   },
   LOGIN: {
@@ -61,9 +31,6 @@ export const ROUTES = {
 export const LINKS = [
   { label: 'About', path: '/about' },
   { label: 'Home', path: '/home' },
-  // { label: 'Events', path: '/events' },
-  // { label: 'Polls', path: '/polls' },
-  // { label: 'Create Poll', path: '/polls/create' },
 ];
 
 export const rootLinks = () => {
