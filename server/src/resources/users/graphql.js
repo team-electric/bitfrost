c813c83ad6657364477adc4dc5bccd5fdecfb304
Jack <ajackofsometrades@gmail.com>
2018-12-11 15:49:20 -0800

    Feature/finishing backend (#15)
    * added example for creating user
    
    * about to add an update user method

--- server/src/resources/users/graphql.js
+++ server/src/resources/users/graphql.js
@@ -89,5 +89,29 @@ export const userMutations = {
       phone,
       address
     }).then(prepare)
-  }
+  },
+  // updateUser: {
+  //   description: 'Update a new user',
+  //   type: UserType,
+  //   args: {
+  //     _id: { type: GraphQLString },
+  //     name: { type: new GraphQLNonNull(GraphQLString) },
+  //     email: { type: GraphQLString },
+  //     phone: { type: GraphQLString },
+  //     address: { type: AddressInputType },
+  //   },
+  //   resolve: (_, {
+  //     _id,
+  //     name,
+  //     email,
+  //     phone,
+  //     address
+  //   }) => User.update({
+  //     _id,
+  //     name,
+  //     email,
+  //     phone,
+  //     address
+  //   }).then(prepare)
+  // }
 }
