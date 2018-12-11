8291f3af1af6eaabf741fb3a57391aee247a0ce3
Davidchhing <davidhchhing@gmail.com>
2018-12-11 13:11:11 -0800

    added button and basic styling to trips container

--- client/src/components/resources/trips/PastTrips.jsx
+++ client/src/components/resources/trips/PastTrips.jsx
@@ -2,6 +2,18 @@ import React, { PureComponent, Fragment } from 'react';
 // import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Nav from '../Nav.jsx';
+import { ROUTES } from '../../../routes/index.js';
+import { Link } from 'react-router-dom';
+
+const MapWrapper = styled.div`
+  width: 100vw;
+  height: 40vh;
+  overflow: hidden;
+  img {
+    width: 100vw;
+    height: auto;
+  }
+`;
 
 const StyledDiv = styled.div`
   h1 {
@@ -9,21 +21,63 @@ const StyledDiv = styled.div`
   }
 `;
 
+const TripsContainer = styled.div`
+    text-align: center;
+    li {
+      list-style: none;
+    }
+    a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.secondary};
+    }
+    width: 90vw;
+`;
+
+const Button = styled.button`
+  background: none;
+  color: inherit;
+  text-align: center;
+  border: 1px solid ${({ theme }) => theme.accentcolor};
+  padding: 15px;
+  font: inherit;
+  cursor: pointer;
+  width: 100%;
+  height: 75px;
+  margin-top: 8px;
+  padding: 15px;
+`;
+
 export default class PastTrips extends PureComponent {
   // static propTypes = {
   //   lots o props
   // }
-
   render() {
+
     return (
       <Fragment>
         <Nav pageTitle="Past Trips" />
-
+        <MapWrapper>
+          <img src="https://staticmapmaker.com/img/google.png"></img>
+        </MapWrapper>
         <StyledDiv>
           <h1>
             Google map with the most recent route shown. user Icon, List of past
             trips here, linking to detail view of a trip.
           </h1>
+          <TripsContainer>
+            <ol>
+              <li> <Link to={ROUTES.TRIPDETAIL.linkTo()}> <Button>David  Montreal, 9.11.21 </Button>
+              </Link></li>
+              <li> <Link to={ROUTES.TRIPDETAIL.linkTo()}> <Button>Michael Corbin  Washginton DC, 11.21.19 </Button>
+              </Link></li>
+              <li> <Link to={ROUTES.TRIPDETAIL.linkTo()}> <Button>Jack from Titanic  London, 9.11.21 </Button>
+              </Link></li>
+
+
+              {/* <li><Link to={ROUTES.TRIPDETAIL.linkTo()}></Link>Jake Pikes place, 9.11.21</li>
+              <li><Link to={ROUTES.TRIPDETAIL.linkTo()}></Link>Ryan New Orleans, 9.11.21</li> */}
+            </ol>
+          </TripsContainer>
         </StyledDiv>
       </Fragment>
     );
