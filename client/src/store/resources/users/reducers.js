2b6ed18819b28feef6b781e9e6815acb1f3daccb
Jack <ajackofsometrades@gmail.com>
2018-12-11 11:05:16 -0800

    set up redux with users service

---
+++ client/src/store/resources/users/reducers.js
@@ -0,0 +1,19 @@
+import { FETCH_USERS, FETCH_USERS_START, FETCH_USERS_DONE } from './actions';
+
+export const initialState = {
+  list: null,
+  loading: false
+};
+
+export default function reducer(state = initialState, { type, payload }) {
+  switch(type) {
+    case FETCH_USERS:
+      return { ...state, list: payload };
+    case FETCH_USERS_START:
+      return { ...state, loading: true };
+    case FETCH_USERS_DONE:
+      return { ...state, loading: false };
+    default:
+      return state;
+  }
+}
