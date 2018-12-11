58b071295643a7cc6439b9704f246c3532e9506e
Ryan Luras <ryanluras@gmail.com>
2018-12-10 18:16:23 -0800

    buttons yo (#6)

--- client/src/components/resources/Dashboard.jsx
+++ client/src/components/resources/Dashboard.jsx
@@ -1,16 +1,40 @@
 import React, { Component, Fragment } from 'react';
 import Nav from './Nav.jsx';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../routes/index.js';
 // import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
-  h1 {
-    font-weight: bolder;
+  font-weight: Extra-Light;
+  font-size: 2em;
+`;
+const ButtonBox = styled.div`
+  width: 100vw;
+  margin: auto;
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+  flex-grow: 2;
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.secondary};
   }
 `;
 
 const Button = styled.button`
-
+  background: none;
+  color: inherit;
+  text-align: center;
+  border: 1px solid ${({ theme }) => theme.accentcolor};
+  padding: 55px;
+  font: inherit;
+  cursor: pointer;
+  width: 40vw;
+  height: 20vh;
+  margin-top: 15px;
 `;
 export default class Dashboard extends Component {
   // static propTypes = {
@@ -23,12 +47,24 @@ export default class Dashboard extends Component {
   render() {
     return (
       <Fragment>
-        <Nav pageTitle='Your Dashboard' />
+        <Nav pageTitle="Your Dashboard" />
+        <ButtonBox>
+          <Link to={ROUTES.UPCOMINGTRIPS.linkTo()}>
+            <Button>Upcoming Trips</Button>
+          </Link>
+          <Link to={ROUTES.PASTTRIPS.linkTo()}>
+            <Button>Past Trips</Button>
+          </Link>
+          <Link to={ROUTES.CREATETRIP.linkTo()}>
+            <Button>Create Trip</Button>
+          </Link>
+          <Link to={ROUTES.PROFILE.linkTo()}>
+            <Button>Favorites</Button>
+          </Link>
+        </ButtonBox>
         <StyledDiv>
-          <h1>
-            User Dashboard here, showing map of current ride markers, user icon,
-            buttons for upcoming trips, past trips, create trip and favorites.
-          </h1>
+          User Dashboard here, showing map of current ride markers, user icon,
+          buttons for upcoming trips, past trips, create trip and favorites.
         </StyledDiv>
       </Fragment>
     );
