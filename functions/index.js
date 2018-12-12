909b3a3f4a06a6f1e126bfbd09d27ab744da6d10
Jack <ajackofsometrades@gmail.com>
2018-12-12 08:43:33 -0800

    initial attempt at cloud function

--- functions/index.js
+++ functions/index.js
@@ -5,20 +5,66 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 exports.updateRide = functions.firestore
-  .document('rides/{userId}')
+  .document('rides/{rideId}')
   .onUpdate((change, context) => {
     const completedRide = change.after.data();
+
     const {
       driver, riders, seats,
       depart, arrive,
       origin, destination
     } = completedRide;
+    const ride = {
+      driver, riders, seats,
+      depart, arrive,
+      origin, destination
+    };
 
-    // send completedRide to mongoDB
-
-
-
-
-    // delete ride from firestore
-
+    createRide(ride);
   });
+
+
+const newRide = ride => JSON.stringify({
+  query:
+    `query {
+      createRide(
+        driver: "${ride.driver}"
+        riders: ["${ride.riders[0]}", "${ride.riders[1]}"]
+        seats: ${ride.seats}
+        comments: "${ride.comments}"
+        origin: "${ride.origin}"
+        destination: { zip: "${ride.zip}" }
+        depart: "${ride.depart}"
+        arrive: "${ride.arrive}"
+      )
+      {
+        _id
+        driver
+        riders
+        seats
+        comments
+        origin
+        destination { zip }
+        depart
+        arrive
+      }
+    }`
+});
+
+const graphqlReq = apiUrl => async(body) => {
+
+  const response = await fetch(
+    apiUrl,
+    {
+      headers: { 'content-type': 'application/json' },
+      method: 'POST',
+      body: body,
+    });
+
+  const responseJson = await response.json();
+  return responseJson.data;
+};
+
+const request = graphqlReq(functions.config().mongodb.url);
+
+const createRide = ride => request(newRide(ride));
