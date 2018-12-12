af36bf116c8e83ce3b1e97c33f8422aa3a478f83
Ryan Luras <ryanluras@gmail.com>
2018-12-12 12:28:36 -0800

    Feature/users (#26)
    * hooking up create user from o-auth signup
    
    * stashing
    
    * stashing

--- client/src/components/resources/SignUp.jsx
+++ client/src/components/resources/SignUp.jsx
@@ -3,7 +3,11 @@ import React, { PureComponent, Fragment } from 'react';
 import styled from 'styled-components';
 var background = require('../../assets/landingwallpaper.jpg');
 import { connect } from 'react-redux';
-import { getUser, getAuth, getUserLoading } from '../../store/resources/users/selectors';
+import {
+  getUser,
+  getAuth,
+  getUserLoading
+} from '../../store/resources/users/selectors';
 import { fetchUser } from '../../store/resources/users/actions';
 import { ROUTES } from '../../routes';
 import { Redirect } from 'react-router-dom';
@@ -105,7 +109,8 @@ class SignUp extends PureComponent {
   };
 
   render() {
-    if(!this.props.loading && this.props.user) return <Redirect to={ROUTES.DASHBOARD.linkTo()} />;
+    if(!this.props.loading && this.props.user)
+      return <Redirect to={ROUTES.DASHBOARD.linkTo()} />;
     if(this.props.loading) return <h1> LOADING </h1>;
 
     return (
@@ -127,22 +132,22 @@ class SignUp extends PureComponent {
           </LabelInputContainer>
           <LabelInputContainer>
             <label>
-              Address{' '}
+              Phone Number
               <input
-                id="address"
-                name="address"
-                type="text"
+                id="phone"
+                name="phone"
+                type="tel"
                 onChange={this.handleChange}
               />
             </label>
           </LabelInputContainer>
           <LabelInputContainer>
             <label>
-              Phone Number
+              Address{' '}
               <input
-                id="phone"
-                name="phone"
-                type="tel"
+                id="address"
+                name="address"
+                type="text"
                 onChange={this.handleChange}
               />
             </label>
