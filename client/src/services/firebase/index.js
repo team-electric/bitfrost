9e231729b24bfc527826d483984f9a45d8cc8605
Jack <ajackofsometrades@gmail.com>
2018-12-12 08:18:05 -0800

    cleanup

--- client/src/services/firebase/index.js
+++ client/src/services/firebase/index.js
@@ -1,10 +1,12 @@
+
+
 const config = {
-  apiKey: 'AIzaSyB3WX_xpDb-KK20xLbeD70evxFOj021NrU',
-  authDomain: 'bitfrost-e04d8.firebaseapp.com',
-  databaseURL: 'https://bitfrost-e04d8.firebaseio.com',
-  projectId: 'bitfrost-e04d8',
-  storageBucket: 'bitfrost-e04d8.appspot.com',
-  messagingSenderId: '1018294573314'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_PROJECT,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 export default config;
--- functions/index.js
+++ functions/index.js
@@ -3,3 +3,22 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Testing testing, 123, I'm with the mic, so let me be");
 });
+
+exports.updateRide = functions.firestore
+  .document('rides/{userId}')
+  .onUpdate((change, context) => {
+    const completedRide = change.after.data();
+    const {
+      driver, riders, seats,
+      depart, arrive,
+      origin, destination
+    } = completedRide;
+
+    // send completedRide to mongoDB
+
+
+
+
+    // delete ride from firestore
+
+  });
