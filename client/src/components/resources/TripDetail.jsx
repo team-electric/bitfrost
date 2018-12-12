60972adf2caaf38e511637f7a4e3b94d68c8d641
Davidchhing <davidhchhing@gmail.com>
2018-12-12 11:17:02 -0800

    removed unnecessary styling

--- client/src/components/resources/TripDetail.jsx
+++ client/src/components/resources/TripDetail.jsx
@@ -32,10 +32,6 @@ const Button = styled.button`
   padding: 15px;
   font: inherit;
   cursor: pointer;
-  /* width: 90%; */
-  /* height: 40px;
-  margin-top: 3px;
-  padding: 10px; */
 `;
 
 const UserInfoContainer = styled.div`
@@ -59,21 +55,13 @@ export default class TripDetail extends Component {
         <MapWrapper>
           <img src="https://staticmapmaker.com/img/google.png"></img>
         </MapWrapper>
-
         <StyledDiv>
-          <h2>
-            Google map with selected trip path shown. Driver Icon and personal
-            information, number, car. Available seats with button to reserve and
-            button to link to paypal/venmo. Trip rating?
-          </h2>
-
           <UserInfoContainer>
             <h3>User Info</h3>
             <div>Name: Thor Ragnarok</div>
             <div>Phone: 971-522-1890</div>
             <div>Email: Thor@Ragnarok.com</div>
           </UserInfoContainer>
-
           <CarInfoContainer>
             <h3>Car Details</h3>
             <div>Make: Lexus</div>
@@ -81,14 +69,11 @@ export default class TripDetail extends Component {
             <div>Plate: 832-JXY</div>
             <div>Seats available: 2</div>
           </CarInfoContainer>
-
           <div>
             <a href="www.paypal.com"> Pay with Paypal</a>
             <a href="www.venmo.com"> Pay with Venmo</a>
           </div>
-
           <Button>Reserve</Button>
-
         </StyledDiv>
       </Fragment>
     );
