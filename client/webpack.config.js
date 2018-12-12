e2ee8c8f31a3c6fe0aa170689a7a0f5be28329b2
Jack <ajackofsometrades@gmail.com>
2018-12-12 10:35:10 -0800

    source-map for prod

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,77 +6,83 @@ const path = require('path');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.[hash].js',
-    publicPath: '/'
-  },
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    port: 8080,
-    historyApiFallback: true,
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:7890',
-        secure: false
-      }
-    }
-  },
-  plugins: [
-    // new CleanPlugin('./dist/bundle.*.js'),
-    new HtmlPlugin({ template: './src/index.html' }),
-    new Dotenv({ path: path.resolve(__dirname, './.env') }),
-    // new BundleAnalyzerPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true
-          }
+module.exports = env => {
+  const isProd = env === 'production';
+
+  return {
+
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.[hash].js',
+      publicPath: '/'
+    },
+    devtool: isProd ? 'source-map' : 'inline-source-map',
+    devServer: {
+      port: 8080,
+      historyApiFallback: true,
+      proxy: {
+        '/graphql': {
+          target: 'http://localhost:7890',
+          secure: false
         }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-            options: { sourceMap: true }
-          },
-          {
-            loader: 'css-loader',
+      }
+    },
+    plugins: [
+      // new CleanPlugin('./dist/bundle.*.js'),
+      new HtmlPlugin({ template: './src/index.html' }),
+      new Dotenv({ path: path.resolve(__dirname, './.env') }),
+      // new BundleAnalyzerPlugin()
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              sourceMap: true,
-              modules: true,
-              importLoaders: 1
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              plugins: [
-                require('autoprefixer')(),
-                require('postcss-nested')(),
-                require('postcss-simple-vars')()
-              ]
+              cacheDirectory: true
             }
           }
-        ]
-      },
-      {
-        test: /\.(jpg|png|svg)$/,
-        use: {
-          loader: 'url-loader',
-          options: { limit: 1000 },
         },
-      }
-    ]
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                modules: true,
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins: [
+                  require('autoprefixer')(),
+                  require('postcss-nested')(),
+                  require('postcss-simple-vars')()
+                ]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(jpg|png|svg)$/,
+          use: {
+            loader: 'url-loader',
+            options: { limit: 1000 },
+          },
+        }
+      ]
+    }
+
   }
+
 };
