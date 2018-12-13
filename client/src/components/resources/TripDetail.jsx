1ce9761c9d3f58894ef29add36f6bbc59d01ffb0
Davidchhing <davidhchhing@gmail.com>
2018-12-13 15:29:46 -0800

    fixed sign up page

--- client/src/components/resources/TripDetail.jsx
+++ client/src/components/resources/TripDetail.jsx
@@ -3,10 +3,40 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import Nav from './Nav.jsx';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import {
+  getUser,
+  getAuth,
+  updateUser
+} from '../../store/resources/users/selectors';
+import { fetchCar } from '../../store/resources/cars/actions';
+import { fetchUser } from '../../store/resources/users/actions';
+import { getUserCar } from '../../store/resources/cars/selectors';
 
 const StyledDiv = styled.div`
   h2 {
     font-weight: bolder;
+  }
+`;
+
+const UserImgWrapper = styled.div`
+  position: relative;
+  margin: auto;
+  margin-bottom: 10px;
+  top: 10px;
+  width: 80px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+`;
+const UserImg = styled.div`
+  width: 80px;
+  border: 2px solid ${({ theme }) => theme.accentcolor};
+  border-radius: 50%;
+  overflow: hidden;
+  img {
+    width: 80px;
   }
 `;
 
@@ -44,19 +74,45 @@ class TripDetail extends Component {
     selectedRide: PropTypes.object.isRequired
   };
 
+  state = {
+    name: '',
+    email: '',
+    phone: ''
+  };
+
+  componentDidMount() {
+    const { name, email, phone } = this.props.user;
+    this.props.fetchCar(this.props.user._id);
+    if(this.props.user) {
+      this.setState({
+        ...this.state,
+        email,
+        name,
+        phone
+      });
+    }
+  }
+
   render() {
+    const { photoURL } = this.props.auth;
     return (
       <Fragment>
-        <Nav pageTitle="Trip Details" />
+        <Nav pageTitle='Trip Details' />
         <MapWrapper>
-          <img src="https://staticmapmaker.com/img/google.png"></img>
+          <img src='https://staticmapmaker.com/img/google.png' />
         </MapWrapper>
+        <UserImgWrapper>
+          <UserImg>
+            <img src={photoURL} />
+          </UserImg>
+        </UserImgWrapper>
+
         <StyledDiv>
           <UserInfoContainer>
             <h3>User Info</h3>
-            <div>Name: Thor Ragnarok</div>
-            <div>Phone: 971-522-1890</div>
-            <div>Email: Thor@Ragnarok.com</div>
+            <div>Name: {this.state.name}</div>
+            <div>Phone: {this.state.phone}</div>
+            <div>Email: {this.state.email}</div>
           </UserInfoContainer>
           <CarInfoContainer>
             <h3>Car Details</h3>
@@ -66,11 +122,10 @@ class TripDetail extends Component {
             <div>Seats available: 2</div>
           </CarInfoContainer>
           <div>
-            <a href="www.paypal.com"> Pay with Paypal</a>
-            <a href="www.venmo.com"> Pay with Venmo</a>
+            <a href='www.paypal.com'> Pay with Paypal</a>
+            <a href='www.venmo.com'> Pay with Venmo</a>
           </div>
           <Button>Reserve</Button>
-
         </StyledDiv>
       </Fragment>
     );
@@ -78,14 +133,36 @@ class TripDetail extends Component {
 }
 
 const mapStateToProps = state => ({
+  uid: state.firebase.auth.uid,
+  ride: state.firestore.ordered.ride || [],
   selectedRide: state.rides.selectedRide,
+  user: getUser(state),
+  auth: getAuth(state),
+  car: getUserCar(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  fetchUser: email => dispatch(fetchUser(email)),
+  fetchCar: userId => dispatch(fetchCar(userId)),
+  selectRide: ({ target }) => {
+    dispatch({ type: 'selectRide', ride: target.value });
+  }
 });
 
-
-export default connect(
-  mapStateToProps, mapDispatchToProps
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  firestoreConnect(props => {
+    if(!props.id) return [];
+    return [
+      {
+        collection: 'rides',
+        where: [['id', '==', props.id]]
+      }
+    ];
+  })
 )(TripDetail);
+
+// props.match.id
