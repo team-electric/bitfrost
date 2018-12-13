56511bd99881df52501e1cf1e90ae0e0dda4b3bb
Jack <ajackofsometrades@gmail.com>
2018-12-13 06:31:46 -0800

    saving progress

--- client/src/components/resources/CreateTrip.jsx
+++ client/src/components/resources/CreateTrip.jsx
@@ -1,8 +1,10 @@
 import React, { PureComponent, Fragment } from 'react';
-import Nav from './Nav.jsx';
-
 // import PropTypes from 'prop-types';
 import styled from 'styled-components';
+
+import Nav from './Nav.jsx';
+import NewRideMap from './maps/NewRideMap.jsx';
+
 
 const StyledForm = styled.form`
   h1 {
@@ -17,23 +19,65 @@ export default class CreateTrip extends PureComponent {
   state = {
     origin: '',
     destination: '',
-    departTime: '',
-    departDate: '',
-    seats: ''
+    depart: null,
+    arrive: null,
   };
 
+  onChange = e => {
+    console.log(this.state);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  createRide = e => {
+    e.preventDefault();
+    const { uid } = this.props;
+
+    const {
+      origin, destination, depart, arrive
+    } = this.state;
+
+    this.props.firestore.add(
+      { collection: 'rides' },
+      { uid, driver, seats }
+    );
+    this.setState({
+      origin: '', destination: '', depart: '', arrive: '',
+    });
+  };
+
+
   render() {
+
+    // save current location as lat,long and send destination as lat,long too
+    // get driver from current user
+    // departed is false
+    // currentlocation is currentlocation
+    // seats from current car
+    // destination from chosen spot
+    const { depart, arrive } = this.state;
+
     return (
       <Fragment>
         <Nav pageTitle="Create A Trip" />
 
-        <StyledForm>
-          <h1>
-            Form to create a trip, importing users database address as default
-            origin or update depart location, and fields for destination
-            address, depart time, comment from driver, import default available
-            seats or update available seats.
-          </h1>
+        <NewRideMap/>
+
+        <StyledForm onSubmit={this.createRide}>
+
+          <label htmlFor="depart">Estimated Depart Time</label>
+          <input
+            type="datetime-local"
+            name="depart" value={depart}
+            onChange={this.onChange}
+          ></input>
+          <label htmlFor="arrive">Estimated Arrival Time</label>
+          <input
+            type="datetime-local"
+            name="arrive" value={arrive}
+            onChange={this.onChange}
+          ></input>
+
+          <button type="submit">Create new trip</button>
         </StyledForm>
       </Fragment>
     );
