3ca6316207c5d3cb82a96611f63487cca2dfda79
Jack <ajackofsometrades@gmail.com>
2018-12-13 13:33:29 -0800

    routing to tripdetail page correctly

--- client/src/components/resources/Dashboard.jsx
+++ client/src/components/resources/Dashboard.jsx
@@ -76,7 +76,7 @@ class Dashboard extends Component {
   static propTypes = {
     uid: PropTypes.string,
     rides: PropTypes.arrayOf(PropTypes.object),
-    selectedRide: PropTypes.string,
+    selectedRide: PropTypes.object,
     selectRide: PropTypes.func.isRequired,
   };
 
@@ -138,10 +138,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logoutUser()),
-  selectRide: ({ target }) => {
-    console.log(target.value);
-    dispatch({ type: 'selectRide', ride: target.value });
-  }
+  selectRide: ride => dispatch({ type: 'selectRide', ride })
 });
 
 export default compose(
