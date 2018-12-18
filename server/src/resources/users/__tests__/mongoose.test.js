69952ba99d9e7b56a776b133cc3bd8217455fe96
Ryan Luras <ryanluras@gmail.com>
2018-12-18 14:25:48 -0800

    added mongo/mongoose User tests for validating schema/model (#69)

--- server/src/resources/users/__tests__/mongoose.test.js
+++ server/src/resources/users/__tests__/mongoose.test.js
@@ -1,34 +1,37 @@
-import { User } from '../mongoose';
-import { getErrors } from '../../../testing/lib/errors';
-// import { mockPoll } from '../../../testing/fixtures/poll';
-const chance = require('chance').Chance();
+const { User } = require("../../users/mongoose");
+const { getErrors } = require("../../../testing/lib/errors");
 
-describe('user model', () => {
-
-  test('dummy', () => {
-
+describe("User Model", () => {
+  it("returns a valid User Schema Model", () => {
+    const data = {
+      name: "joe",
+      email: "joe@email.com",
+      phone: "345-345-3456",
+      address: {
+        street: "123 Some St.",
+        city: "Portland",
+        state: "OR",
+        zip: "97202"
+      }
+    };
+    const user = new User(data);
+    const jsonUser = user.toJSON();
+    expect(jsonUser).toEqual({ ...data, _id: expect.any(Object) });
   });
 
-  // test('validates a good model', () => {
-  //   const data = mockPoll();
-  //   const poll = new Poll(data);
-  //   const jsonPoll = poll.toJSON();
+  it("validates that fields are required", () => {
+    const user = new User({});
 
-  //   expect(jsonPoll).toEqual({
-  //     ...data,
-  //     _id: expect.any(Object),
-  //     choices: data.choices.map(choice => ({ ...choice, _id: expect.any(Object) }))
-  //   });
-  // });
+    const errors = getErrors(user.validateSync(), 7);
 
-  // test('requires title, description, and choices', () => {
-  //   const poll = new Poll({});
-  //   const errors = getErrors(poll.validateSync(), 3);
+    expect(errors.name.properties.message).toEqual("Name is required");
+    expect(errors.email.properties.message).toEqual("Email is required");
+    expect(errors.phone.properties.message).toEqual("Phone is required");
+    expect(errors['address.city'].properties.message).toEqual("City is required");
+    expect(errors['address.street'].properties.message).toEqual("Street is required");
+    expect(errors['address.state'].properties.message).toEqual("State is required");
+    expect(errors['address.zip'].properties.message).toEqual("ZIP code is required");
 
-  //   expect(errors.title.properties.message).toEqual('Path `title` is required.');
-  //   expect(errors.description.properties.message).toEqual('Path `description` is required.');
-  //   expect(errors.choices.properties.message).toEqual('Validator failed for path `choices` with value ``');
-  // });
-
+  });
 
 });
--- server/src/resources/users/mongoose.js
+++ server/src/resources/users/mongoose.js
@@ -3,22 +3,29 @@ import { model, Schema } from 'mongoose';
 const userSchema = Schema({
   name: {
     type: String,
-    required: true
+    required: [true, 'Name is required']
   },
   email: {
     type: String,
-    required: true
+    required: [true, 'Email is required']
   },
   phone: {
     type: String,
-    required: true
+    required: [true, 'Phone is required']
   },
   address: {
-    street: String,
-    city: String,
+    street: {
+      type: String,
+      required: [true, 'Street is required'],
+  },
+    city: {
+      type: String,
+      required: [true, 'City is required'],
+  },
     state: {
         type: String,
-        enum: ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY']
+        enum: ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'],
+        required: [true, 'State is required'],
     },
     zip: {
         type: String,
